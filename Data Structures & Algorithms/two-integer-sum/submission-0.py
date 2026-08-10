3a583c56cd7801df6d62b07d4e33f049fbe7c6dd
Vijul Tyagi <92642078+vijultyagi@users.noreply.github.com>
2026-08-09 23:13:03 -0400

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -0,0 +1,7 @@
+# T:O(n^2), S:O(1)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if target == nums[i] + nums[j]:
+                    return [i,j]
