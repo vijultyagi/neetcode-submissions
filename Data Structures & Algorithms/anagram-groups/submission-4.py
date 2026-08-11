22971fb6114da96a6c3e3868b7704d366231f407
Vijul Tyagi <92642078+vijultyagi@users.noreply.github.com>
2026-08-10 23:32:45 -0400

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,13 @@
+# T:O(m*nlogn) S:O(n*m)
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dict = {}
+        for item in strs:
+            sort_n = "".join(sorted(item))
+            if sort_n in dict:
+                dict[sort_n].append(item)
+            else:
+                dict[sort_n] = [item]
+
+        return list(dict.values())
+        
